56738254b2880829dcfd571240f95340d24c525a
Dai <baboiletta@gmail.com>
2020-04-15 09:49:51 +0700

    tam

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -25,17 +25,19 @@ class PostDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    id
   user
   categories
-  id
+  title
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    id
   user
+  title
   categories
-  id
   title
   content
   slug
